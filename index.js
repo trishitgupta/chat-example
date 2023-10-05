686834ef4fe2e98f8eac64152d457021481706cb
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-10-05 15:39:20 +0200

    feat: initial commit for step #7

--- index.js
+++ index.js
@@ -2,23 +2,49 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {}
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+async function main() {
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
   });
-});
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', async (msg) => {
+      let result;
+      try {
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        // TODO handle the failure
+        return;
+      }
+      io.emit('chat message', msg, result.lastID);
+    });
+  });
+
+  server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+  });
+}
+
+main();
